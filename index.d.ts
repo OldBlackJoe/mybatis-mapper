348ff65c0e6e59d2694cd4e4b4e48709ca70a2b7
Павел Овсянников <paawaaa@gmail.com>
2018-11-06 15:06:15 +0400

    Formatted index.d.ts using prettier

--- index.d.ts
+++ index.d.ts
@@ -1,22 +1,33 @@
 declare namespace MybatisMapper {
-
   export type MyBatisMapper = {
     [name: string]: MyBatisMapper | Array<number | string | null | boolean>;
   };
 
   export type Params = {
-    [name: string]: string | string[] | number | number[] | boolean | null | undefined;
+    [name: string]:
+      | string
+      | string[]
+      | number
+      | number[]
+      | boolean
+      | null
+      | undefined;
   };
 
   export type Format = {
-    language?: 'sql' | 'db2' | 'n1ql' | 'pl/sql';
+    language?: "sql" | "db2" | "n1ql" | "pl/sql";
     indent?: string;
     params?: Params;
   };
 
   export function createMapper(xmls: string[]): void;
 
-  export function getStatement(namespace: string, sql: string, param?: Params, format?: Format): string;
+  export function getStatement(
+    namespace: string,
+    sql: string,
+    param?: Params,
+    format?: Format
+  ): string;
 
   export function getMapper(): MyBatisMapper;
 }
