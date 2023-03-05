450fddea619e1aab4a1a69341e3d108baf205b70
Tsdy <571219188@qq.com>
2023-03-05 22:59:03 +0800

    fix: Fix trim suffix may be empty and param may have underscore

---
+++ test/testunderline.js
@@ -0,0 +1,26 @@
+var mybatisMapper = require('../index');
+mybatisMapper.createMapper([ './test/testunderline.xml' ]);
+var assert = require('assert');
+
+describe("Unit Tests for Issue of mavridiSS", function(){
+  it("1) test for Issue of mavridiSS", function(done){
+
+    var param = {
+        page_size : 10,
+        page: 1,
+        price : 100
+    }
+    var format = {language: 'sql', indent: ' '};
+    var query = mybatisMapper.getStatement('testUnderline', 'findOne', param, format);
+    assert.equal(query,
+`
+SELECT
+ *
+FROM
+ article
+WHERE
+ name = '10'
+`.trim())
+    done();
+  });
+});
